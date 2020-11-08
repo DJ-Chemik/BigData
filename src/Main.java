5a9fb7faaeb8f69e2e10d2773ff0bca2a09157bb
dj-chemik <dj-chemik@wp.pl>
2020-11-08 16:55:16 +0100

    AvgSizeCycleStation

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 import java.io.IOException;
 import java.util.regex.Pattern;
 import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.io.DoubleWritable;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
 import org.apache.hadoop.mapreduce.Job;
@@ -39,32 +40,52 @@ public class Main extends Configured implements Tool {
 
     public static class Map extends Mapper<LongWritable, Text, Text, IntWritable> {
         private final static IntWritable one = new IntWritable(1);
-        private Text word = new Text();
+        private Text wyearord = new Text();
         private long numRecords = 0;
         private static final Pattern WORD_BOUNDARY = Pattern.compile("\\s*\\b\\s*");
+        private Text year = new Text();
+        private IntWritable size = new IntWritable();
 
-        public void map(LongWritable offset, Text lineText, Context context) throws IOException, InterruptedException {
-            String line = lineText.toString();
-            Text currentWord = new Text();
-            for (String word : WORD_BOUNDARY.split(line)) {
-                if (word.isEmpty()) {
-                    continue;
+        public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+            if (key.get() == 0)
+                return;
+            else {
+                String line = value.toString();
+                int i = 0;
+                for (String word : line.split(",(?=([^\"]*\"[^\"]*\")*[^\"]*$)")) {
+                    if (i == 4) {
+                        year.set(word.substring(word.lastIndexOf('/') + 1,
+                                word.lastIndexOf('/') + 5));
+                    }
+                    if (i == 5) {
+                        size.set(Integer.parseInt(word));
+                    }
+                    i++;
                 }
-                currentWord = new Text(word);
-                context.write(currentWord,one);
+                context.write(year, size);
             }
         }
     }
 
-    public static class Reduce extends Reducer<Text, IntWritable, Text, IntWritable> {
+    public static class Reduce extends Reducer<Text, IntWritable, Text, DoubleWritable> {
+        private DoubleWritable result = new DoubleWritable();
+        Float average;
+        Float count;
+        int sum;
+
         @Override
-        public void reduce(Text word, Iterable<IntWritable> counts, Context context)
-                throws IOException, InterruptedException {
-            int sum = 0;
-            for (IntWritable count : counts) {
-                sum += count.get();
+        public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+            average = 0f;
+            count = 0f;
+            sum = 0;
+            Text sumText = new Text("average size of station for " + key + " year is: ");
+            for (IntWritable val : values) {
+                sum += val.get();
+                count += 1;
             }
-            context.write(word, new IntWritable(sum));
+            average = sum / count;
+            result.set(average);
+            context.write(sumText, result);
         }
     }
 }
